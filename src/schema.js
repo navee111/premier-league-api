d39e06c812c42ba1000f6ee4f6677cc86b9def82
Navid <nh222yr@student.lnu.se>
2026-03-02 17:23:29 +0100

    add  schema create graphQL

---
+++ src/schema.js
@@ -0,0 +1,51 @@
+import { qql } from 'apollo-server-express'
+
+export const typeDefs = qql`
+  type Team {
+    id: Int!
+    name: String!
+    players: [Player!]!
+  }
+
+  type Player {
+    id: Int!
+    name: String!
+    goals: Int!
+    assists: Int!
+    team: Team!
+  }
+
+  type Match {
+    id: Int!
+    homeTeam: String!
+    awayTeam: String!
+    homeScore: Int!
+    awayScore: Int!
+    matchDate: String!
+    comments: [MatchComment!]!
+  }
+
+  type MatchComment {
+    id: Int!
+    content: String!
+    rating: Int!
+    createdAt: String!
+    match: Match!
+  }
+
+  type User {
+    id: Int!
+    email: String!
+  }
+
+  type Query {
+    teams: [Team!]!
+    players: [Player!]!
+    matches: [Match!]!
+    match(id: Int!): Match
+  }
+
+  type Mutation {
+    createMatchComment(matchId: Int!, content: String!, rating: Int!): MatchComment!
+}
+`
